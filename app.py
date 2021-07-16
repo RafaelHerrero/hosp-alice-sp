ed843293f4e91cf130f5bc978cfa81d7cb9c7d71
Rafael Herrero <rafaelgherrero@gmail.com>
2021-07-16 17:19:56 -0300

    Mudança de mapa devido a implementação de novas Features
    Adicionadas as features de Busca, Layer e Popup

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def render_the_map():
-    return render_template('mapa_hosp_sp_distritos.html')
+    return render_template('mapa_hosp_sp_distritos_rev01.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
